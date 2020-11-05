27a3961ec0783da92f503c6c6b4a037171618154
Vini Speck <vinicius.speck@catolicasc.org.br>
2020-11-05 18:32:41 -0300

    Finalização do teste dos métodos

--- src/ShapeTest.java
+++ src/ShapeTest.java
@@ -1,6 +1,21 @@
 public class ShapeTest {
     public static void main(String[] args) {
-        //Shape shape = new Circle();
+        Shape shape = new Shape("blue", false);
+        Circle circle = new Circle("black", false, 4.5);
+        Rectangle rectangle = new Rectangle("white", true, 1.4, 22.5);
+        Square square = new Square("red", true, 28.1, 9.4,1.2);
+
+        System.out.println("" + shape);
+        System.out.println("" + circle);
+        System.out.println("" + rectangle);
+        System.out.println("" + square);
+
+        System.out.println("" + circle.getPerimeter());
+        System.out.println("" + circle.getArea());
+
+        System.out.println("" + rectangle.getPerimeter());
+        System.out.println("" + rectangle.getArea());
+
 
     }
 }
--- src/Square.java
+++ src/Square.java
@@ -1,10 +1,22 @@
 public class Square extends Rectangle{
-    public Square(){
-
-    }
-
-    public Square(String color, boolean filled, double width, double length) {
+    double side;
+    public Square(String color, boolean filled, double width, double length, double side) {
         super(color, filled, width, length);
+        setSide(side);
     }
 
+    public double getSide() {
+        return side;
+    }
+
+    public void setSide(double side) {
+        this.side = side;
+        setLength(side);
+        setWidth(side);
+    }
+
+    @Override
+    public String toString() {
+        return "Square[" + super.toString() + "]";
+    }
 }
